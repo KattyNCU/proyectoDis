42f2d68c60d43b988a83b63f684c5d210054159d
Katty <katttys000@gmail.com>
2021-08-07 19:11:15 -0400

    Agregar proyecto

---
+++ Assets/avtar/Scripts/Salir.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Salir : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void SaliR()
+    {
+        Application.Quit();
+        Debug.Log("Saliste del juego");
+    }
+} 
---
+++ Assets/movimiento.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimiento : MonoBehaviour
+{
+    public Transform vrCamera;
+    public float toggleAngle = 30.0f;
+    public float speed = 3.0f;
+    public bool movi;
+
+    private CharacterController cc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (vrCamera.eulerAngles.x >= toggleAngle && vrCamera.eulerAngles.x < 90.0f)
+        { movi = true; }
+        else { movi = false; }
+        if (movi)
+
+        {
+            Vector3 forward = vrCamera.TransformDirection(Vector3.forward);
+            cc.SimpleMove(forward * speed);
+        }
+    }
+}
